70613be886dc17aa3ca7b001a84ec3152e67ebe6
St-andrey <40577453+St-andrey@users.noreply.github.com>
2019-11-22 02:55:09 +0300

    header and slider started

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,9 @@
 $(function(){
-
-
-
-  
+    $('.works-slider').slick({
+        dots: false,
+        arrows: true,
+        slidesToShow: 1,
+        prevArrow: ('.works-slider__arrows--left'),
+        nextArrow: ('.works-slider__arrows--right'),
+    })
 });
